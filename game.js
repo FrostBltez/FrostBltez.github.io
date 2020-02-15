b1363953a0817f1932894e17c3495a037eae5183
FrostBltez <59379041+FrostBltez@users.noreply.github.com>
2020-02-14 19:55:42 -0600

    First website upload!

---
+++ game.js
@@ -0,0 +1,17 @@
+//Game Core//
+const d = document
+const Prodsr = d.getElementById("ProductionScreen")
+
+const ProductionPage = new Array(Prodsr)
+//Variables
+var darkEnergy = 0
+var Darkagent = 0
+
+//Pages
+function OpenPage(){
+  for(i=0;i<ProductionPage.length;i++){
+    ProductionPage[i].hidden = false
+    }
+  }
+    
+    

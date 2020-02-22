e541b28b67c7398a1fdde98fc623b2833c2afaca
FrostBltez <59379041+FrostBltez@users.noreply.github.com>
2020-02-22 13:28:08 -0600

    Update GameManager.js

--- GameManager.js
+++ GameManager.js
@@ -1,3 +1,7 @@
 function Game_Main(){
-    console.log("I am the console, I speak for the program!")
+    console.log("I am the console, I speak for the program!");
 }
+
+window.addEventListener('load', function(event){
+Game_Main()
+});

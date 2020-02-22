51c93259a6c8ac4a8262f4a03b8209d5c5313d9d
FrostBltez <59379041+FrostBltez@users.noreply.github.com>
2020-02-22 13:33:16 -0600

    Update GameManager.js

--- GameManager.js
+++ GameManager.js
@@ -1,4 +1,16 @@
+function Game_Init(){
+window.addEventListener('keydown', function(evt){
+console.log("Key Pressed")
+});
+
+
+
+console.log("GAME WORKING!")
+}
+
+
 function Game_Main(){
+    Game_Init();
     console.log("I am the console, I speak for the program!");
 }
 
